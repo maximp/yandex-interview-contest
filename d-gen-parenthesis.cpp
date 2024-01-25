76c31bb4d55aed29f12ae69c50283fce2ac4a597
Maxim Perenesenko <171828@gmail.com>
2024-01-25 13:28:34 +0300

    Update d-gen-parenthesis.cpp

--- d-gen-parenthesis.cpp
+++ d-gen-parenthesis.cpp
@@ -7,18 +7,18 @@ void gen(int n, int counter_open, int counter_close, string ans)
 {
 	if(counter_open + counter_close == 2 * n)
    	{
-    	cout << ans << endl;
-    	return;
+    		cout << ans << endl;
+    		return;
 	}
    	if(counter_open < n)
-    	gen(n, counter_open + 1, counter_close, ans + '(');
+    		gen(n, counter_open + 1, counter_close, ans + '(');
    	if(counter_open > counter_close)
-    	gen(n, counter_open, counter_close + 1, ans + ')');
+    		gen(n, counter_open, counter_close + 1, ans + ')');
 }
 
 int main()
 {
-	int n = 0;
+    int n = 0;
     cin >> n;
     gen(n, 0, 0, "");
 	return 0;
